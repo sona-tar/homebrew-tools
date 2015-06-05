ec28da4149e1e33605bdbae00f5094ed8b1a51fd
sona-tar <sona.zip@gmail.com>
2015-06-05 13:39:36 +0000

    Update 0.0.4

--- ghs.rb
+++ ghs.rb
@@ -1,23 +1,23 @@
 require 'formula'
 
-VERSION='0.0.3'
+VERSION='0.0.4'
 class Ghs < Formula
   homepage 'https://github.com/sona-tar/ghs'
   if OS.mac?
     if Hardware.is_64_bit?
       url "https://github.com/sona-tar/ghs/releases/download/#{VERSION}/ghs-#{VERSION}-darwin_amd64.zip"
-      sha1 '55668c47e1dba9a2a4dbda02e69698ab83052dc4'
+      sha1 'ab0108bd18978138690325e2501a7b3b53611622'
     else
       url "https://github.com/sona-tar/ghs/releases/download/#{VERSION}/ghs-#{VERSION}-darwin_386.zip"
-      sha1 '55dd1ea979bfc32abf099d45d848e1f1e97544b7'
+      sha1 'ddabab2637eeeca35e37c7bebd0d0e34b272249e'
     end
   elsif OS.linux?
     if Hardware.is_64_bit?
       url "https://github.com/sona-tar/ghs/releases/download/#{VERSION}/ghs-#{VERSION}-linux_amd64.tar.gz"
-      sha1 '6ef6a5de959085e909d76f8b5d3088b8abf16286'
+      sha1 '3495553f74ba82c5d53b345643b915479ed1327e'
     else
       url "https://github.com/sona-tar/ghs/releases/download/#{VERSION}/ghs-#{VERSION}-linux_386.tar.gz"
-      sha1 '5453a2389782ffffef2a4e593b9ac0005d215392'
+      sha1 'b669202c5bec99dfb827c478aa1d7e5534409895'
     end
   end
 
