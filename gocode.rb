75bbcebc226bff7fe05cbb50311dd9a8cd62d616
sona-tar <sona.zip@gmail.com>
2015-10-24 17:46:13 +0900

    Add gocode fomula

---
+++ gocode.rb
@@ -0,0 +1,21 @@
+require "language/go"
+
+class Gocode < Formula
+  homepage "https://github.com/nsf/gocode"
+  url  "https://github.com/nsf/gocode", :using => :git, :branch => "master"
+  head "https://github.com/nsf/gocode", :using => :git, :branch => "master"
+
+  depends_on "go" => :build
+  depends_on :hg => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    mkdir_p buildpath/"src/github.com/nsf/gocode"
+    ln_sf buildpath, buildpath/"src/github.com/nsf/gocode"
+    Language::Go.stage_deps resources, buildpath/"src"
+
+    # Build and install
+    system "go", "build", "-o", "gocode"
+    bin.install "gocode"
+  end
+end
