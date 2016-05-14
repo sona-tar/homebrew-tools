5f9c86e1aa0e2b74077bfc5a1acb3d71e9f87878
sona-tar <sona.zip@gmail.com>
2016-05-14 22:00:17 +0900

    Update 0.0.9

--- ghs.rb
+++ ghs.rb
@@ -2,7 +2,7 @@ require "language/go"
 
 class Ghs < Formula
   homepage "https://github.com/sona-tar/ghs"
-  url  "https://github.com/sona-tar/ghs", :using => :git, :tag => "0.0.8"
+  url  "https://github.com/sona-tar/ghs", :using => :git, :tag => "0.0.9"
   head "https://github.com/sona-tar/ghs", :using => :git, :branch => "master"
 
   depends_on "go" => :build
